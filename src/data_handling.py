4e922f44a9e13694b541aee1a6191ddef01f00b1
noapr <pariente@campus.technion.ac.il>
2023-11-02 19:16:09 +0200

    refactor directory name

--- src/data_handling.py
+++ src/data_handling.py
@@ -5,8 +5,8 @@ import torchvision.transforms as transforms
 
 def load_cifar10():
     transform = transforms.Compose([transforms.ToTensor(), ])
-    train_set = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
-    test_set = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
+    train_set = torchvision.datasets.CIFAR10(root='../dataset', train=True, download=True, transform=transform)
+    test_set = torchvision.datasets.CIFAR10(root='../dataset', train=False, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True, num_workers=2)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=False, num_workers=2)
     return train_loader, test_loader
@@ -17,8 +17,8 @@ def create_bw_color_pairs():
 
     for i, (color_image, label) in enumerate(train_loader, 0):
         grayscale_image = torch.mean(color_image, dim=1, keepdim=True)
-        torchvision.utils.save_image(grayscale_image, f'../data/bw_images/bw_image_{i}.png')
-        torchvision.utils.save_image(color_image, f'../data/color_images/color_image_{i}.png')
+        torchvision.utils.save_image(grayscale_image, f'../dataset/bw_images/bw_image_{i}.png')
+        torchvision.utils.save_image(color_image, f'../dataset/color_images/color_image_{i}.png')
 
 
 if __name__ == '__main__':
